71db4118695f9ae7addb857d840b77ee1ae2e4f5
emirekrembarutcu <emirekrembarutcu@gmail.com>
2021-08-17 17:14:13 +0300

    v.1.2
    New layers and overlayers were added. New tools were added.

--- src/js/Exercise-2.js
+++ src/js/Exercise-2.js
@@ -1,76 +1,85 @@
 
-var zoomLevel = 6
-var latitude = 39.712
-var longitude = 34.392
+var zoomLevel = 6       //
+var latitude = 39.254   // The opening coordinates and zoom level
+var longitude = 35.829  //
 
-var map = L.map('map',{zoomControl:false}).setView([latitude, longitude], zoomLevel);
+var map = L.map('map',{zoomControl:false}).setView([latitude, longitude], zoomLevel); // Map variable which arrange the opening parameters
 
 var OSM = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '<a>Emir Ekrem Barutçu | </a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    attribution: '<a title="Creator of this website" href="https://www.linkedin.com/in/emirekrembarutcu">Emir Ekrem Barutçu | </a> &copy; ' +
+        '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map); // Adding the OSM and attribution to map
+
+
+
+L.control.betterscale().addTo(map); // Adding the Scale to the map
+
+map.addControl(new L.Control.Fullscreen()); //Fullscreen controller
+
+L.control.polylineMeasure(options = {showClearControl: true, showUnitControl: true,currentCircle: {
+        color: '#000',              // Color of the border of the circle
+        weight: 1,                  // Weight of the circle
+        fillColor: '#00ffea',          // Fill color of the circle
+        fillOpacity: 1,             // Fill opacity of the circle
+        radius: 5                   // Radius of the circle
+    },}
+).addTo(map);   // Adding the polyline tool to the map
 
 var zoomControler = L.control.zoom({
-    zoomInText :"+",
-    zoomInTitle:"Zoom In",
-    zoomOutText:"-",
-    zoomOutTitle:"Zoom Out",
-    position:"topright"
-})
-zoomControler.addTo(map)
+})  // Zoom controller variable
+zoomControler.addTo(map) // Adding the zoom controller to map
 
-var zoomLocation = zoomControler.getPosition();
+var OSM_topo = L.tileLayer.wms("https://ows.terrestris.de/osm/service", {
+    layers:"TOPO-WMS",
+    format:"image/png",
+    transparent: true,
+    attributionİ:"World Topographic Map"
+});
+var SRTM_colored_hillshade = L.tileLayer.wms("https://ows.terrestris.de/osm/service", {
+    layers:"SRTM30-Colored-Hillshade",
+    format:"image/png",
+    transparent: true,
+    attributionİ:"SRTM30 Colored Hillshade Map"
+});
 
-zoomControler.enable();
-//zoomControler.remove();
-
-var mapAttribution = L.control.attribution({
-    prefix:'<a href="https://web.itu.edu.tr/barutcue17/me.html" target = "_blank">Emir Ekrem Barutçu</a>',
-    position:'topleft'});
-mapAttribution.addTo(map);
-mapAttribution.setPosition("bottomleft");
-mapAttribution.remove();
-zoomControler.setPosition("topleft");
-
-var uyduharitasi = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{subdomains:['mt0','mt1','mt2','mt3']});
-var googleharitasi = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{subdomains:['mt0','mt1','mt2','mt3']});
 var srtm_tur = L.tileLayer.wms("https://tucbs-public-api.csb.gov.tr/trk_srtm_wms", {
     layers: '0',
     format: 'image/png',
     transparent: true,
     attribution: "Turkey SRTM Data"
-});
+}); //Adding the Turkey SRTM WMS as a layer to the map
 var cog_ad = L.tileLayer.wms("https://tucbs-public-api.csb.gov.tr/trk_cya_wms", {
     layers: '0',
     format: 'image/png',
     transparent: true,
     attribution: "İl Sınırları"
-});
+}); // Adding the Turkey Province Boundaries WMS as a layer to the map
 var grids_25 = L.tileLayer.wms("https://tucbs-public-api.csb.gov.tr/trk_cografigrid_wms", {
     layers: '0',
     format: 'image/png',
     transparent: true,
     attribution: "25K Grids"
-});
+}); //Adding the 25K grids of TURKEY WMS as a layer to the map
 
 var grids_100 = L.tileLayer.wms("https://tucbs-public-api.csb.gov.tr/trk_cografigrid_wms", {
     layers: '1',
     format: 'image/png',
     transparent: true,
     attribution: "100K Grids"
-});
+}); //Adding the 100K grids of TURKEY WMS as a layer to the map
 var baseLayers;
 baseLayers = {
-    "Uydu Haritasi": uyduharitasi,
-    "Google Haritasi": googleharitasi,
+
     "Open Street Map": OSM,
-    "İl Sınırları": cog_ad,
-};
+    "Topographic Map": OSM_topo,
+    "Boundaries of Provinces in Turkey": cog_ad,
+
+}; // Creating the layer group
 
 var overlays = {
-    "Grid 25K": grids_25,
-    "Grid 100K": grids_100,
-    "Türkiye SRTM": srtm_tur,
-}
-var AllLayers = L.control.layers(baseLayers, overlays).addTo(map);
-L.control.betterscale().addTo(map);
-
+    "Turkey Grid 25K": grids_25,
+    "Turkey Grid 100K": grids_100,
+    "Turkey SRTM Elevation Data": srtm_tur,
+    "SRTM30 Colored Hillshade":SRTM_colored_hillshade,
+} // Creating the overlayer group
+var AllLayers = L.control.layers(baseLayers, overlays).addTo(map); // Adding the layer groups to the map
